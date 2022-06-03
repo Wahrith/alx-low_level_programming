326b1bf2d00fc2c4b3bfb77316395eff6928351f
Wahrith <warithtimi01@gmail.com>
2022-06-03 12:50:36 +0000

    abeg

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,20 +2,26 @@
 
 /**
  * main - Entry point
- * Return:0
+ *
+ * Return: Always 0 (Succes)
  */
+
 int main(void)
 {
-	int i, y;
-	for (i = 0; i < 10; i++)
-	{
-		putchar (i + '0');
-	}
-	for (y = 'a'; y <= 'f'; y++)
-	{
-		putchar (y);
-	}
+	int n;
 
-putchar('\n');
-return (0);
+	for (n = 0 ; n < 16 ; n++)
+	{
+		if (n < 10)
+		{
+			putchar('0' + n);
+		}
+		else
+		{
+			putchar(87 + n);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
+
